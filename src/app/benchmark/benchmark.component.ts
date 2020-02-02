86fc634a715da5678f35f23bc36ee0ae02d1b654
Mattias Levlin <mattias.levlin@gmail.com>
2020-02-02 23:11:29 +0200

    Implement add 10,000 elements and remove

--- src/app/benchmark/benchmark.component.ts
+++ src/app/benchmark/benchmark.component.ts
@@ -5,9 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './benchmark.component.html',
   styleUrls: ['./benchmark.component.css']
 })
-export class BenchmarkComponent implements OnInit {
 
+
+export class BenchmarkComponent implements OnInit {
+  htmlToAdd = ''
   constructor() { }
+
+  add10000Elements() {
+    this.htmlToAdd = [...Array(10000)].map((e, i) =>
+      '<div class="">div #' + i + '</div>'
+    ).join('')
+  }
+
+  remove10000Elements() {
+    this.htmlToAdd = ''
+  }
+
 
   ngOnInit() {
   }
