c6124698db21678e8e9061ad41e9272567b3f0d0
Mattias Levlin <mattias.levlin@gmail.com>
2020-02-02 22:16:06 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BenchmarkComponent } from './benchmark/benchmark.component';
+import { BenchmarkContainerComponent } from './benchmark-container/benchmark-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BenchmarkComponent,
+    BenchmarkContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/benchmark-container/benchmark-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-benchmark-container',
+  templateUrl: './benchmark-container.component.html',
+  styleUrls: ['./benchmark-container.component.css']
+})
+export class BenchmarkContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
