11062525e779438eb7b749210a214af8c5cb0453
Mattias Levlin <mattias.levlin@gmail.com>
2020-04-04 23:47:15 +0300

    Add performance.now() test and remove .svg stuff

--- src/app/benchmark/benchmark.component.ts
+++ src/app/benchmark/benchmark.component.ts
@@ -6,48 +6,31 @@ import { ViewChild, ElementRef, Component, OnInit, Renderer2 } from '@angular/co
   styleUrls: ['./benchmark.component.css']
 })
 
-
 export class BenchmarkComponent {
   @ViewChild('benchmarkContainer', {static: false}) benchmarkContainer:ElementRef;
   htmlToAdd = ''
+  asdf1 = 0
+  asdf2 = 0
   constructor(private renderer: Renderer2) { }
 
-  addEmptyDivs() {
-    for (var i = 0; i <10000; i+=1) {
-      var asd = this.renderer.createElement('div');
-      this.renderer.setAttribute(asd, 'id', i);
-      this.renderer.appendChild(this.benchmarkContainer.nativeElement, asd);
-    }
+  ngAfterViewChecked() {
+    this.asdf2 = performance.now()
+    console.log('This shit took ', (this.asdf2 - this.asdf1), ' milliseconds')
   }
 
+  // 1 ADD DIVS
   addDivs() {
+    this.asdf1 = performance.now()
     for (var i = 0; i <10000; i+=1) {
       var asd = this.renderer.createElement('div');
-      this.renderer.setAttribute(asd, 'id', i);
+      this.renderer.setAttribute(asd, 'id', i.toString());
       var text = this.renderer.createText('asd')
-      this.renderer.appendChild(asd, text)
+      this.renderer.appendChild(asd, text);
       this.renderer.appendChild(this.benchmarkContainer.nativeElement, asd);
     }
   }
 
-  addEmptySvgs() {
-    for (var i = 0; i <10000; i+=1) {
-      var asd = this.renderer.createElement('svg');
-      this.renderer.setAttribute(asd, 'id', i);
-      this.renderer.appendChild(this.benchmarkContainer.nativeElement, asd);
-    }
-  }
-
-  addSvgs() {
-    for (var i = 0; i <10000; i+=1) {
-      var asd = this.renderer.createElement('svg');
-      this.renderer.setAttribute(asd, 'id', i);
-      var text = this.renderer.createText('asd')
-      this.renderer.appendChild(asd, text)
-      this.renderer.appendChild(this.benchmarkContainer.nativeElement, asd);
-    }
-  }
-
+  // 5 REMOVE ALL
   remove() {
     const childElements = this.benchmarkContainer.nativeElement.childNodes;
     console.log(childElements.length)
